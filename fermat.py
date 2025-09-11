49f81f67f6ae47a031848084961725d8b99c0729
DrPacar <lukapacar11@gmail.com>
2025-09-11 08:13:41 +0200

    implement more compact way to calculate fermat algorithm, add example output

--- fermat.py
+++ fermat.py
@@ -1,5 +1,5 @@
 __author__ = "Luka Pacar"
-
+from collections import Counter
 
 def get_fermat_statistics(n: int) -> str:
     """
@@ -7,13 +7,29 @@ def get_fermat_statistics(n: int) -> str:
     :param n: the number to get fermat-statistics from.
     :return: number -> percentage_to_be_prime -> number_of_is_prime_results, number_of_different_results - map of all number results
     """
-    output_map = {}
+    c = Counter()
     for i in range(1, n):
-        fermat_result = (i ** (n-1)) % n
-        output_map[fermat_result] = output_map.get(fermat_result, 0) + 1
+        fermat_result = (i ** (n - 1)) % n
+        c[fermat_result] += 1
 
-    return f"{n} -> {(output_map[1]/(n-1) * 100):.0f}% -> res[1]={output_map[1]}, len(res)={len(output_map)} - {list(output_map.items())}"
+    return f"{n} -> {(c[1] / (n - 1) * 100):.0f}% -> res[1]={c[1]}, len(res)={len(c)} - {list(c.items())}"
+
+def is_prim_millerrabin(n):
+
+
 
 if __name__ == "__main__":
-    print(get_fermat_statistics(556))
-    pass
+    # Aufgabe 1:
+    # Berechne (in Python) für jede der Primzahlen 𝑝 = 2 bis 𝑝 = 11 und 𝑝 = 997:
+    print("Aufgabe 1:")
+    print(get_fermat_statistics(2))
+    print(get_fermat_statistics(11))
+    print(get_fermat_statistics(997))
+    print()
+    # Alle 3 sind primzahlen (es kommt immer zu 100% 1 raus)
+
+    # Aufgabe 2:
+    # Was passiert bei Nicht-Primzahlen?
+    for n in [2, 3, 5, 7, 11, 997, 9, 15, 21, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565,
+              566, 567, 568, 569, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 6601, 8911]:
+        print(get_fermat_statistics(n))
