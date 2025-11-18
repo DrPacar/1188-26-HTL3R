84fadd4a2d5de46f7b32818ec84e32a07a6c4df3
DrPacar <lukapacar06@gmail.com>
2025-11-18 13:42:22 +0100

    labyrinth.py: implement further typing and docs

--- UE02_Labyrinth/labyrinth.py
+++ UE02_Labyrinth/labyrinth.py
@@ -3,13 +3,13 @@ from collections import deque
 
 __author__ = "Luka Pacar"
 
-def traverse_breadth_first_search(map: list[list[str]], start_pos: tuple[int, int], directions: dict[str, tuple[int, int]]) -> tuple[tuple[int, int], list[tuple[int, int]]]:
+def traverse_breadth_first_search(grid: list[list[str]], start_pos: tuple[int, int], directions: dict[str, tuple[int, int]]) -> tuple[tuple[int, int], list[tuple[int, int]]] | None:
     """
     Traverses the graph using Breadth First Search (Breitensuche).
-    :param map: Map to traverse.
+    :param grid: Map to traverse.
     :param start_pos: The starting position in the map.
     :param directions: The possible directions to take
-    :return: [0] -> Ending-Point/Destination - [1] -> Path to the destination
+    :return: [0] -> Ending-Point/Destination - [1] -> Path to the destination | Or None if there is no way to the destination
     """
     queue = deque()
     visited_points = set()
@@ -19,58 +19,60 @@ def traverse_breadth_first_search(map: list[list[str]], start_pos: tuple[int, in
         curr_path = curr_path.copy()
         curr_path.append(curr_pos)
 
-        if curr_pos in visited_points or map[curr_pos[0]][curr_pos[1]] == '#':
+        if curr_pos in visited_points or grid[curr_pos[0]][curr_pos[1]] == '#':
             continue
         visited_points.add(curr_pos)
 
-        if map[curr_pos[0]][curr_pos[1]] == 'A':
+        if grid[curr_pos[0]][curr_pos[1]] == 'A':
             return curr_pos, curr_path
 
         for _, vector in directions.items():
             new_pos = (curr_pos[0] + vector[0], curr_pos[1] + vector[1])
-            if is_out_of_bounce(map, new_pos) or new_pos in visited_points or map[new_pos[0]][new_pos[1]] == 'X':
+            if is_out_of_bounce(grid, new_pos) or new_pos in visited_points or grid[new_pos[0]][new_pos[1]] == 'X':
                 continue
             queue.append((new_pos, curr_path))
 
-def traverse_depth_first_search(map: list[list[str]], curr_pos: tuple[int, int], directions, curr_path, visited_points) -> tuple[tuple[int, int], list[tuple[int, int]]]:
+def traverse_depth_first_search(grid: list[list[str]], curr_pos: tuple[int, int], directions: dict[str, tuple[int, int]], curr_path: list[tuple[int, int]], visited_points: set[tuple[int, int]]) -> tuple[tuple[int, int], list[tuple[int, int]]] | None:
     """
         Traverses the graph using Depth First Search (Tiefensuche).
-        :param map: Map to traverse.
+        :param grid: Map to traverse.
         :param curr_pos: The current position in the map.
-        :param directions: The possible directions to take
-        :return: [0] -> Ending-Point/Destination - [1] -> Path to the destination
+        :param directions: The possible directions to take.
+        :param curr_path: The current path taken to the curr_pos.
+        :param visited_points: The current visited points up until this point.
+        :return: [0] -> Ending-Point/Destination - [1] -> Path to the destination | Or None if there is no way to the destination.
         """
-    if is_out_of_bounce(map, curr_pos):
+    if is_out_of_bounce(grid, curr_pos):
         return None
 
-    if curr_pos in visited_points or map[curr_pos[0]][curr_pos[1]] == '#':
+    if curr_pos in visited_points or grid[curr_pos[0]][curr_pos[1]] == '#':
         return None
     visited_points.add(curr_pos)
 
     curr_path.append(curr_pos)
 
-    if map[curr_pos[0]][curr_pos[1]] == 'A':
+    if grid[curr_pos[0]][curr_pos[1]] == 'A':
         return curr_pos, curr_path
 
     for _, vector in directions.items():
         new_pos = (curr_pos[0] + vector[0], curr_pos[1] + vector[1])
         copied_path = curr_path.copy()
-        result = traverse_depth_first_search(map, new_pos, directions, copied_path, visited_points)
+        result = traverse_depth_first_search(grid, new_pos, directions, copied_path, visited_points)
         if result is not None:
             return result
 
     return None
 
-def is_out_of_bounce(map: list[list[str]], curr_pos: tuple[int, int]) -> bool:
+def is_out_of_bounce(grid: list[list[str]], curr_pos: tuple[int, int]) -> bool:
     """
     Checks if a given point is outside the given map
-    :param map: The given map.
+    :param grid: The given map.
     :param curr_pos: The position to test.
     :return: True if the given value is out of bounce, otherwise False
     """
-    return curr_pos[0] < 0 or curr_pos[0] >= len(map) or curr_pos[1] < 0 or curr_pos[1] >= len(map[0])
+    return curr_pos[0] < 0 or curr_pos[0] >= len(grid) or curr_pos[1] < 0 or curr_pos[1] >= len(grid[0])
 
-directions = {
+pos_directions = {
     "UP": (-1,0),
     "RIGHT": (0, 1),
     "DOWN": (1, 0),
@@ -91,16 +93,16 @@ def to_map(map_lines: list[str]) -> list[list[str]]:
         output.append(curr_line)
     return output
 
-def print_map(map: list[list[str]]):
+def print_map(grid: list[list[str]]):
     """ Prints the given map. """
-    for line in map:
+    for line in grid:
         for char in line:
             print(char, end ="")
 
-def mark_map(map, path):
+def mark_map(grid, given_path):
     """Marks the given path in the given map."""
-    for node in path:
-        map[node[0]][node[1]] = 'O'
+    for node in given_path:
+        grid[node[0]][node[1]] = 'O'
 
 if __name__ == "__main__":
     for file in ["l1", "l2", "l3"]:
@@ -112,15 +114,14 @@ if __name__ == "__main__":
         grid_breadth_first_search = to_map(lines).copy()
 
         # DFS
-        result, path = traverse_depth_first_search(grid_depth_first_search, (1,1), directions, [], set())
+        results1, path = traverse_depth_first_search(grid_depth_first_search, (1,1), pos_directions, [], set())
         mark_map(grid_depth_first_search, path)
         grid_depth_first_search[1][1] = 'S'
 
         print_map(grid_depth_first_search)
-
         print()
         # BFS
-        result, path = traverse_breadth_first_search(grid_breadth_first_search, (1, 1), directions)
+        results2, path = traverse_breadth_first_search(grid_breadth_first_search, (1, 1), pos_directions)
         mark_map(grid_breadth_first_search, path)
         grid_breadth_first_search[1][1] = 'S'
 
